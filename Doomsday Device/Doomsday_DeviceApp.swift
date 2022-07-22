2a499e76ea02680992ebb582bd833015752008a1
paytondev <coocothegreat12345@gmail.com>
2022-07-21 23:08:01 -0400

    Initial Commit

---
+++ Doomsday Device/Doomsday_DeviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doomsday_DeviceApp.swift
+//  Doomsday Device
+//
+//  Created by Payton Curry on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Doomsday_DeviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
